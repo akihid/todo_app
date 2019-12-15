703879c87d71bfc76544521a1dfc4d5174aed499
akihide sato <akihide315@gmail.com>
2019-12-15 20:23:10 +0900

    Add: userテーブルにカラム追加

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ Route::get('/', 'HomeController@index')->name('home');
 
 Route::resource('listings', 'ListingController')->except(['index']);
 Route::resource('/listings/{listing}/tasks', 'TaskController');
+Route::resource('users', 'UserController', ['only' => ['edit', 'update', 'show']]);
